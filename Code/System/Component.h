2c4905d9f12dd3505230a77f4d6d82f1ee147d6e
Hansol Park <mooming.go@gmail.com>
2017-09-01 11:37:09 +0900

    Update Component
    Working on Component

--- Code/System/Component.h
+++ Code/System/Component.h
@@ -9,27 +9,9 @@ namespace HE
 {
     class Component
     {
-        // ==================================================================================
-        // Life Cycle
-        // 0. Born
-        // 1. Init
-        // 2. OnEnable
-        // 3. Update
-        // 4. OnDisable
-        // 5. OnEnable
-        // 6. Update
-        // 7. OnDisable
-        // 8. Release
-        // 9. Death
-        //
-        // State
-        // 1. Born
-        // 2. Alive
-        // 3. Sleep
-        // 4. Dead
 
     public:
-        enum State
+        enum class State
         {
             NONE,
             BORN,
@@ -45,6 +27,7 @@ namespace HE
     public:
         virtual void Init() = 0;
         virtual void Update(const float deltaTime) = 0;
+        virtual void PostUpdate(const float deltaTime) = 0;
         virtual void Release() = 0;
 
         virtual void OnEnable() = 0;
@@ -52,33 +35,23 @@ namespace HE
 
     public:
         inline Component(const char* name) : state(NONE), name(name) {}
-        inline Component(const Component& rhs) : state(rhs.state), name(rhs.name) {}
-        inline Component(Component&& rhs) : state(rhs.state), name(std::move(rhs.name)) {}
         virtual ~Component() = default;
-
-        Component& operator= (const Component& rhs)
-        {
-            state = rhs.state;
-            name = rhs.name;
-            return *this;
-        }
-
-        Component& operator= (Component&& rhs)
-        {
-            state = rhs.state;
-            name = std::move(rhs.name);
-
-            return *this;
-        }
 
         inline State GetState() const { return state; }
         inline void SetState(State state) { Component::state = state; }
 
-        inline const char* GetName() const { return name.ToCharArray(); }
+        inline const String& GetName() const { return name; }
         inline bool IsEnabled() const { return state == ALIVE; }
 
-        inline void SetEnable(bool isEnabled) { state = isEnabled ? ALIVE : SLEEP; }
-        inline void Destroy() { state = DEAD; }
+        inline void SetEnable(bool isEnabled)
+        {
+            SetState(isEnabled ? ALIVE : SLEEP);
+        }
+        
+        inline void Destroy()
+        {
+            SetState(State.DEAD);
+        }
     };
 }
 
