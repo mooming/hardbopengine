b074314da67180f5643b1f8008781cf8bfe0a6a0
Hansol Park <mooming.go@gmail.com>
2025-11-24 23:06:10 +0000

    Added a vulkan rendering example

--- Applications/EngineTest/TestMain.cpp
+++ Applications/EngineTest/TestMain.cpp
@@ -3,9 +3,6 @@
 #include "Engine/Engine.h"
 #include "Test/UnitTestCollection.h"
 
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
 int main(int argc, const char* argv[])
 {
     hbe::Engine hengine;
---
+++ Applications/VulkanTest/Main.cpp
@@ -0,0 +1,127 @@
+// Created by mooming.go@gmail.com
+
+#include "Engine/Engine.h"
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+
+namespace hbe
+{
+enum class EDisplayMode : uint8_t
+{
+	Window,
+	FullScreen,
+	WindowedFullScreen
+};
+
+class WindowApplication
+{
+private:
+	EDisplayMode displayMode;
+	bool isResizable;
+	int width;
+	int height;
+	StaticString title;
+	GLFWwindow* window;
+
+public:
+	WindowApplication(const char* title)
+		: displayMode(EDisplayMode::FullScreen)
+		, isResizable(true)
+		, width(-1)
+		, height(-1)
+		, title(title)
+		, window(nullptr)
+	{
+	}
+
+	void SetDisplayMode(EDisplayMode mode)
+	{
+		displayMode = mode;
+	}
+
+	void SetResolution(int inWidth, int inHeight)
+	{
+		width = inWidth;
+		height = inHeight;
+	}
+
+	void Initialize()
+	{
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		auto monitor = glfwGetPrimaryMonitor();
+		const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+		glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+		glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+		glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+		glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+
+		if (width < 0)
+		{
+			width = mode->width;
+		}
+
+		if (height < 0)
+		{
+			height = mode->height;
+		}
+
+		window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+
+		switch (displayMode)
+		{
+			case EDisplayMode::Window:
+				glfwSetWindowMonitor(window, nullptr, 0, 0, width, height, mode->refreshRate);
+				break;
+			case EDisplayMode::FullScreen:
+				glfwSetWindowMonitor(window, monitor, 0, 0, width, height, mode->refreshRate);
+				break;
+			case EDisplayMode::WindowedFullScreen:
+				glfwSetWindowMonitor(window, monitor, 0, 0, width, height, mode->refreshRate);
+				break;
+		}
+
+		glfwSetWindowAttrib(window, GLFW_RESIZABLE, isResizable ? GLFW_TRUE : GLFW_FALSE);
+	}
+
+	void Update()
+	{
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+	}
+
+	void Cleanup()
+	{
+		glfwDestroyWindow(window);
+
+		glfwTerminate();
+	}
+};
+} // namespace hbbe
+
+int main(int argc, const char* argv[])
+{
+	using namespace hbe;
+
+    Engine hengine;
+    hengine.Initialize(argc, argv);
+
+	WindowApplication app("Vulkan Example");
+
+	app.SetDisplayMode(EDisplayMode::Window);
+	app.Initialize();
+	app.Update();
+	app.Cleanup();
+
+    hengine.ShutDown();
+    hengine.WaitForEnd();
+
+    return 0;
+}
