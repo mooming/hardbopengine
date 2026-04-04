4303637027747f353396f51ca3290fd52c1f9b89
Hansol Park <mooming.go@gmail.com>
2026-04-05 01:48:59 +0900

    Revmoved VulkanTriangle example and update VulkanSDK install scripts

--- Applications/EngineTest/TestMain.cpp
+++ Applications/EngineTest/TestMain.cpp
@@ -3,13 +3,6 @@
 #include "Engine/Engine.h"
 #include "Test/UnitTestCollection.h"
 
-#if VULKAN_SDK
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-#else
-// Vulkan SDK not available - test will run without Vulkan support
-#endif
-
 int main(int argc, const char* argv[])
 {
     hbe::Engine hengine;
