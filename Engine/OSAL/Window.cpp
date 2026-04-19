7183b757d6b3be1965e844da1ce5f6e6173ad59b
Hansol Park <mooming.go@gmail.com>
2026-04-20 02:58:52 +0900

    test: implement WindowTest with proper assertions and error reporting
    - Implement empty test cases: Set/Get Size, Visibility, Poll Events, Close Window
    - Remove non-functional ShouldClose test (no interface method exists)
    - Add assertions using if statements with lferr for error reporting
    - Add mutable keyword to lambda for this capture
    - Keep both app->PollEvents() and window->PollEvents() for multi-window support

--- Engine/OSAL/Window.cpp
+++ Engine/OSAL/Window.cpp
@@ -75,13 +75,14 @@ void WindowTest::Prepare()
 
 		Data userData{this, ls, windowPromise};
 
-		taskSystem.DispatchToMainThread([](void* userData)
+		taskSystem.DispatchToMainThread([](void* userData) mutable
 		{
 			auto data = static_cast<Data*>(userData);
 			FatalAssert(data != nullptr);
 
 			auto& ls = data->ls;
 			auto& lf = data->thisObject->lf;
+			auto& lferr = data->thisObject->lferr;
 			auto& windowPromise = data->windowPromise;
 
 			auto app = Engine::Get().GetApplication();
@@ -94,18 +95,37 @@ void WindowTest::Prepare()
 			auto window = OS::CreateWindow("Test Window", 800, 600);
 			if (!window)
 			{
-				ls << "Failed to create a window" << lf;
+				ls << "Failed to create a window" << lferr;
 				windowPromise.set_value(std::move(window));
 				return;
 			}
 
-			ls << "Width: " << window->GetWidth() << lf;
-			ls << "Height: " << window->GetHeight() << lf;
+			const int width = window->GetWidth();
+			const int height = window->GetHeight();
+			ls << "Width: " << width << lf;
+			ls << "Height: " << height << lf;
+
+			if (width != 800)
+			{
+				ls << "Invalid width: " << width << ", expected 800" << lferr;
+			}
+
+			if (height != 600)
+			{
+				ls << "Invalid height: " << height << ", expected 600" << lferr;
+			}
+
+			if (!window->IsVisible())
+			{
+				ls << "Window should be visible" << lferr;
+			}
+
 			ls << "A window has been created successfully" << lf;
 
 			for (int i = 0; i < 100; ++i)
 			{
 				app->PollEvents();
+				window->PollEvents();
 				std::this_thread::sleep_for(std::chrono::milliseconds(10));
 			}
 
@@ -147,20 +167,40 @@ void WindowTest::Prepare()
 
 			auto& ls = data->ls;
 			auto& lf = data->thisObject->lf;
+			auto& lferr = data->thisObject->lferr;
 			auto& windowPromise = data->windowPromise;
 
 			auto window = OS::CreateWindow("Initial Title", 800, 600);
 			if (!window)
 			{
-				ls << "Failed to create a window" << lf;
+				ls << "Failed to create a window" << lferr;
 				windowPromise.set_value(std::move(window));
 				return;
 			}
 
-			ls << "Width: " << window->GetWidth() << lf;
-			ls << "Height: " << window->GetHeight() << lf;
+			const int width = window->GetWidth();
+			const int height = window->GetHeight();
+			ls << "Width: " << width << lf;
+			ls << "Height: " << height << lf;
+
+			if (width != 800)
+			{
+				ls << "Invalid width: " << width << ", expected 800" << lferr;
+			}
+
+			if (height != 600)
+			{
+				ls << "Invalid height: " << height << ", expected 600" << lferr;
+			}
+
+			if (!window->IsVisible())
+			{
+				ls << "Window should be visible" << lferr;
+			}
+
 			ls << "A window has been created successfully" << lf;
 
+			bool titleSet = false;
 			for (int i = 0; i < 100; ++i)
 			{
 				if (i == 50)
@@ -168,9 +208,101 @@ void WindowTest::Prepare()
 					hbe::HString newTitle = "New Title";
 					window->SetTitle(newTitle);
 					ls << "Title set to: " << newTitle.c_str() << lf;
+					titleSet = true;
 				}
 
 				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			}
+
+			if (!titleSet)
+				ls << "Failed to set title" << lf;
+
+			window.reset();
+			windowPromise.set_value(nullptr);
+		}, &userData);
+
+		windowFuture.get();
+	});
+
+	AddTest("Set and Get Size", [this](auto& ls)
+	{
+		auto& taskSystem = Engine::Get().GetTaskSystem();
+		std::promise<std::unique_ptr<OS::IWindow>> windowPromise;
+		auto windowFuture = windowPromise.get_future();
+
+		using TThis = decltype(this);
+		using TLs = decltype(ls);
+		using TPromise = decltype(windowPromise);
+
+		struct Data final
+		{
+			TThis thisObject;
+			TLs& ls;
+			TPromise& windowPromise;
+
+			Data(TThis thisObj, TLs& ls, TPromise& promise)
+				: thisObject(thisObj), ls(ls), windowPromise(promise)
+			{
+			}
+		};
+
+		Data userData{this, ls, windowPromise};
+
+		taskSystem.DispatchToMainThread([](void* userData) mutable
+		{
+			auto data = static_cast<Data*>(userData);
+			FatalAssert(data != nullptr);
+
+			auto& ls = data->ls;
+			auto& lf = data->thisObject->lf;
+			auto& lferr = data->thisObject->lferr;
+			auto& windowPromise = data->windowPromise;
+
+			auto window = OS::CreateWindow("Resize Test", 800, 600);
+			if (!window)
+			{
+				ls << "Failed to create a window" << lferr;
+				windowPromise.set_value(std::move(window));
+				return;
+			}
+
+			const int initialWidth = window->GetWidth();
+			const int initialHeight = window->GetHeight();
+			ls << "Initial Size: " << initialWidth << "x" << initialHeight << lf;
+
+			if (initialWidth != 800)
+			{
+				ls << "Invalid initial width: " << initialWidth << ", expected 800" << lferr;
+			}
+			if (initialHeight != 600)
+			{
+				ls << "Invalid initial height: " << initialHeight << ", expected 600" << lferr;
+			}
+
+			for (int i = 0; i < 100; ++i)
+			{
+				if (i == 50)
+				{
+					const int newWidth = 1024;
+					const int newHeight = 768;
+					window->SetSize(newWidth, newHeight);
+					ls << "Resizing to: " << newWidth << "x" << newHeight << lf;
+				}
+
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			}
+
+			const int finalWidth = window->GetWidth();
+			const int finalHeight = window->GetHeight();
+			ls << "Final Size: " << finalWidth << "x" << finalHeight << lf;
+
+			if (finalWidth != 1024)
+			{
+				ls << "Invalid final width: " << finalWidth << ", expected 1024" << lferr;
+			}
+			if (finalHeight != 768)
+			{
+				ls << "Invalid final height: " << finalHeight << ", expected 768" << lferr;
 			}
 
 			window.reset();
@@ -180,28 +312,264 @@ void WindowTest::Prepare()
 		windowFuture.get();
 	});
 
-	AddTest("Set and Get Size", [](auto& ls)
+	AddTest("Visibility", [this](auto& ls)
 	{
+		auto& taskSystem = Engine::Get().GetTaskSystem();
+		std::promise<std::unique_ptr<OS::IWindow>> windowPromise;
+		auto windowFuture = windowPromise.get_future();
 
+		using TThis = decltype(this);
+		using TLs = decltype(ls);
+		using TPromise = decltype(windowPromise);
+
+		struct Data final
+		{
+			TThis thisObject;
+			TLs& ls;
+			TPromise& windowPromise;
+
+			Data(TThis thisObj, TLs& ls, TPromise& promise)
+				: thisObject(thisObj), ls(ls), windowPromise(promise)
+			{
+			}
+		};
+
+		Data userData{this, ls, windowPromise};
+
+		taskSystem.DispatchToMainThread([](void* userData) mutable
+		{
+			auto data = static_cast<Data*>(userData);
+			FatalAssert(data != nullptr);
+
+			auto& ls = data->ls;
+			auto& lf = data->thisObject->lf;
+			auto& lferr = data->thisObject->lferr;
+			auto& windowPromise = data->windowPromise;
+
+			auto window = OS::CreateWindow("Visibility Test", 800, 600);
+			if (!window)
+			{
+				ls << "Failed to create a window" << lferr;
+				windowPromise.set_value(std::move(window));
+				return;
+			}
+
+			const bool initiallyVisible = window->IsVisible();
+			ls << "Initially visible: " << (initiallyVisible ? "true" : "false") << lf;
+
+			if (!initiallyVisible)
+			{
+				ls << "Window should be initially visible" << lferr;
+			}
+
+			for (int i = 0; i < 100; ++i)
+			{
+				if (i == 25)
+				{
+					window->SetVisible(false);
+					ls << "Set visible: false" << lf;
+				}
+				else if (i == 50)
+				{
+					const bool isHidden = !window->IsVisible();
+					ls << "Is visible after hide: " << (isHidden ? "false" : "true") << lf;
+
+					if (window->IsVisible())
+					{
+						ls << "Window should be hidden after SetVisible(false)" << lferr;
+					}
+				}
+				else if (i == 75)
+				{
+					window->SetVisible(true);
+					ls << "Set visible: true" << lf;
+				}
+
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			}
+
+			const bool finalVisible = window->IsVisible();
+			ls << "Final visible: " << (finalVisible ? "true" : "false") << lf;
+
+			if (!finalVisible)
+			{
+				ls << "Window should be visible at the end" << lferr;
+			}
+
+			window.reset();
+			windowPromise.set_value(nullptr);
+		}, &userData);
+
+		windowFuture.get();
 	});
 
-	AddTest("Visibility", [](auto& ls)
+	AddTest("Poll Events", [this](auto& ls)
 	{
+		auto& taskSystem = Engine::Get().GetTaskSystem();
+		std::promise<std::unique_ptr<OS::IWindow>> windowPromise;
+		auto windowFuture = windowPromise.get_future();
+
+		using TThis = decltype(this);
+		using TLs = decltype(ls);
+		using TPromise = decltype(windowPromise);
+
+		struct Data final
+		{
+			TThis thisObject;
+			TLs& ls;
+			TPromise& windowPromise;
+
+			Data(TThis thisObj, TLs& ls, TPromise& promise)
+				: thisObject(thisObj), ls(ls), windowPromise(promise)
+			{
+			}
+		};
+
+		Data userData{this, ls, windowPromise};
+
+		taskSystem.DispatchToMainThread([](void* userData) mutable
+		{
+			auto data = static_cast<Data*>(userData);
+			FatalAssert(data != nullptr);
+
+			auto& ls = data->ls;
+			auto& lf = data->thisObject->lf;
+			auto& lferr = data->thisObject->lferr;
+			auto& windowPromise = data->windowPromise;
+
+			auto window = OS::CreateWindow("Poll Events Test", 800, 600);
+			if (!window)
+			{
+				ls << "Failed to create a window" << lferr;
+				windowPromise.set_value(std::move(window));
+				return;
+			}
+
+			auto app = Engine::Get().GetApplication();
+			if (!app)
+			{
+				ls << "Failed to get an application instance" << lferr;
+				window.reset();
+				windowPromise.set_value(nullptr);
+				return;
+			}
+
+			ls << "Starting to poll events..." << lf;
+
+			for (int i = 0; i < 50; ++i)
+			{
+				app->PollEvents();
+				window->PollEvents();
+
+				if (i % 10 == 0)
+				{
+					ls << "Poll iteration: " << i << lf;
+				}
+
+				if (window->GetWidth() != 800)
+				{
+					ls << "Invalid width during poll: " << window->GetWidth() << ", expected 800" << lferr;
+				}
+
+				if (window->GetHeight() != 600)
+				{
+					ls << "Invalid height during poll: " << window->GetHeight() << ", expected 600" << lferr;
+				}
+
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			}
+
+			ls << "Poll events completed successfully" << lf;
+
+			if (window->GetWidth() != 800)
+			{
+				ls << "Invalid width after poll: " << window->GetWidth() << ", expected 800" << lferr;
+			}
+
+			if (window->GetHeight() != 600)
+			{
+				ls << "Invalid height after poll: " << window->GetHeight() << ", expected 600" << lferr;
+			}
+
+			window.reset();
+			windowPromise.set_value(nullptr);
+		}, &userData);
+
+		windowFuture.get();
 	});
 
-	AddTest("Poll Events", [](auto& ls)
+	AddTest("Close Window", [this](auto& ls)
 	{
+		auto& taskSystem = Engine::Get().GetTaskSystem();
+		std::promise<std::unique_ptr<OS::IWindow>> windowPromise;
+		auto windowFuture = windowPromise.get_future();
 
-	});
+		using TThis = decltype(this);
+		using TLs = decltype(ls);
+		using TPromise = decltype(windowPromise);
 
-	AddTest("ShouldClose", [](auto& ls)
-	{
+		struct Data final
+		{
+			TThis thisObject;
+			TLs& ls;
+			TPromise& windowPromise;
 
-	});
+			Data(TThis thisObj, TLs& ls, TPromise& promise)
+				: thisObject(thisObj), ls(ls), windowPromise(promise)
+			{
+			}
+		};
 
-	AddTest("Close Window", [](auto& ls)
-	{
+		Data userData{this, ls, windowPromise};
 
+		taskSystem.DispatchToMainThread([](void* userData) mutable
+		{
+			auto data = static_cast<Data*>(userData);
+			FatalAssert(data != nullptr);
+
+			auto& ls = data->ls;
+			auto& lf = data->thisObject->lf;
+			auto& lferr = data->thisObject->lferr;
+			auto& windowPromise = data->windowPromise;
+
+			auto window = OS::CreateWindow("Close Test", 800, 600);
+			if (!window)
+			{
+				ls << "Failed to create a window" << lferr;
+				windowPromise.set_value(std::move(window));
+				return;
+			}
+
+			ls << "Window created, closing..." << lf;
+
+			if (window->GetWidth() != 800)
+			{
+				ls << "Invalid width: " << window->GetWidth() << ", expected 800" << lferr;
+			}
+
+			if (window->GetHeight() != 600)
+			{
+				ls << "Invalid height: " << window->GetHeight() << ", expected 600" << lferr;
+			}
+
+			if (!window->IsVisible())
+			{
+				ls << "Window should be visible" << lferr;
+			}
+
+			for (int i = 0; i < 20; ++i)
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			}
+
+			window->Close();
+			ls << "Window closed" << lf;
+
+			window.reset();
+			windowPromise.set_value(nullptr);
+		}, &userData);
+
+		windowFuture.get();
 	});
 }
 
